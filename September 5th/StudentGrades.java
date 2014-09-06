da2f4e974914199af6c8a415a3728156235536a1
Jeffrey Enamorado <jenamorado@ymail.com>
2014-09-06 08:19:07 -0400

    StudentGrades
    Make up 5 names and data for them while using + operator

--- September 5th/StudentGrades.java
+++ September 5th/StudentGrades.java
@@ -1,6 +1,3 @@
-//********************************************************************
-//  StudentGrades.java
-//
 //  Prints a table of points students earned in lab work.
 //*******************************************************************
 
@@ -12,13 +9,17 @@ public class StudentGrades
     //--------------------------------------------------------
     public static void main (String[] args)
     {
-		//Print Boarder
-		
-		
-		
-		//Print Table
-		
-		
+  //Print Boarder
+   System.out.println (" ///////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+     System.out.println (" ==\tStudent Points\t       ==");
+       System.out.println (" \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\///////////////////");
+      
+  //Print Table
+       System.out.println ();
+          System.out.println ("Name\tLab\tBonus\tTotal");
+       System.out.println ("----\t---\t-----\t-----");
+       System.out.println ("Juan\t54\t6\t60" + "\nCarlos\t67\t5\t72" + "\nRacheal\t59\t7\t66" + "\nHelen\t76\t2\t78" + "\nMaria\t67\t5\t72");
+       
+  
     }
 }
-
