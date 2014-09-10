328eddcaa5c77b631cff9d26f4b5b064d1fd0c4f
Jeffrey Enamorado <jenamorado@ymail.com>
2014-09-10 10:02:57 -0400

    Work Uncomplete
    Missing 6

---
+++ Average.java
@@ -0,0 +1,29 @@
+// ***************************************************************** 
+// Average.java
+//
+// Read three integers from the user and print their average
+// ***************************************************************** 
+import java.util.Scanner; 
+public class Average 
+{
+public static void main(String[] args) 
+{
+int val1, val2, val3;
+double average;
+double sum;
+Scanner scan = new Scanner(System.in) ;
+// get three values from user
+   System.out.println("Please enter three integers and " + "I will compute their average");
+   val1 = scan.nextInt();
+   val2 = scan.nextInt();
+   val3 = scan.nextInt();
+  
+//compute the average
+   sum =  (val1 + val2 + val3);
+   System.out.println ("Sum of all data points is " + sum );
+   average = (sum / 3);
+//print the average
+   System.out.println ("Average equals " + average);
+
+} 
+}
